a0c38c29ea46ff408ebbc3494fc261d982d4f62c
opilane <opilane@HP-CZC2349HTN>
2024-11-04 15:47:48 +0200

    Add project files.

---
+++ Triangle_Vorm/Form1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Triangle_Vorm
+{
+     public partial class Form1 : Form
+    {
+
+        Button btn;
+         public Form1()
+        {
+            this.Height = 800;
+            this.Width = 400;
+            this.Text = "Triangle Vorm";
+
+            btn = new Button();
+            btn.Text = "Käivitada";
+            btn.BackColor = Color.GreenYellow;
+            btn.Height = 50;
+            btn.Width = 70;
+            btn.Location = new Point(150, 50);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Triangle_Vorm/Triangle.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Vorm
+{
+    internal class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        public double h;
+
+        public Triangle(double A, double B, double C, double H)
+        {
+            a = A;
+            b = B;
+            c = C;
+            h = H;
+        }
+
+        public string outputA()
+            { return Convert.ToString(a); }
+
+        public string outputB()
+        { return Convert.ToString(b); }
+
+        public string outputC()
+        { return Convert.ToString(c); }
+
+        public string outputH()
+        { return Convert.ToString(h); }
+
+
+        public double Perimetr()
+            { double p = 0;
+            p = a + b + c;
+            return p; }
+
+
+        public double Surface()
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s =Math.Sqrt((p *(p-a)*(p-b)*(p-c)));
+            return s;
+        }
+
+        public double Ruut()
+        {
+            double s = 0;
+            s = (h * a) / 2;
+            return s;
+
+        }
+
+
+
+
+
+
+        public double GetSetA
+        {   get { return a; } 
+            set { a = value; }
+        }
+
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public double GetSetH
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public bool ExistTriangle
+            { get 
+            {   if ((a > b + c) && (b > a + c) && (c > b + a))
+                return false;
+                else  return true ;
+            } 
+        
+        
+        }
+
+
+    }
+}
